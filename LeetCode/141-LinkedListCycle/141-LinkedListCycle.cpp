f1e2f00b6a27672e9853bcb364a22210c47aa190
harsh <harsh.nocturnalking@gmail.com>
2021-10-26 01:06:51 -0500

    Solution
    Solution

--- LeetCode/141-LinkedListCycle/141-LinkedListCycle.cpp
+++ LeetCode/141-LinkedListCycle/141-LinkedListCycle.cpp
@@ -11,7 +11,30 @@ class Solution {
 public:
 	bool hasCycle(ListNode *head) {
 		ListNode* fast, *slow;
-		if(head== nullptr)
+		
+		if (head == nullptr || head->next == nullptr)
+		{
+			return false;
+		}
+		
+		fast = head;
+		slow = head;
+
+		while (fast != nullptr)
+		{
+			if (fast->next != nullptr && fast->next->next != nullptr)
+			{
+				fast = fast->next->next;
+				slow = slow->next;
+				if (fast == slow)
+					return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return false;
 	}
 };
 
