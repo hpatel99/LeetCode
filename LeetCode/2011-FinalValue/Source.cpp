c5478ad3222db41707164c0c862af320b5f663b5
harsh <harsh.nocturnalking@gmail.com>
2021-12-22 03:38:08 -0600

    Update Source.cpp
    Code

--- LeetCode/2011-FinalValue/Source.cpp
+++ LeetCode/2011-FinalValue/Source.cpp
@@ -5,7 +5,14 @@ using namespace std;
 class Solution {
 public:
 	int finalValueAfterOperations(vector<string>& operations) {
+		int answer =0;
 
+		for (string s : operations)
+		{
+			answer += s[1] == '+' ? 1 : -1;
+		}
+
+		return answer;
 	}
 };
 
