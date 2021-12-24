7400a323b940d2bbd7f945479604c1d1d63d635a
harsh <harsh.nocturnalking@gmail.com>
2021-12-24 03:27:59 -0600

    Solution

--- LeetCode/771-JewelAndStone/Source.cpp
+++ LeetCode/771-JewelAndStone/Source.cpp
@@ -1,4 +1,5 @@
 #include<string>
+#include<unordered_set>
 
 using namespace std;
 
@@ -6,10 +7,30 @@ class Solution {
 public:
 	int numJewelsInStones(string jewels, string stones) {
 
+		unordered_set<char> JewelSet;
+		int answer = 0;
+
+		for (int i = 0; i < jewels.size(); i++)
+		{
+			JewelSet.emplace(jewels[i]);
+		}
+
+		for (int i = 0; i < stones.size(); i++)
+		{
+			if (JewelSet.find(stones[i]) != JewelSet.end())
+			{
+				++answer;
+			}
+
+		}
+
+		return answer;
 	}
 };
 
 int main()
 {
+	Solution *s = new Solution();
+	s->numJewelsInStones("aA", "aAAbBB");
 
 }
