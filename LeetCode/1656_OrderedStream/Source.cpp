4597bf94d64f3562ec79d4a788f1322d091e7827
harsh <harsh.nocturnalking@gmail.com>
2021-12-27 00:29:12 -0600

    Initial Setup

---
+++ LeetCode/1656_OrderedStream/Source.cpp
@@ -0,0 +1,15 @@
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class OrderedStream {
+public:
+	OrderedStream(int n) {
+
+	}
+
+	vector<string> insert(int idKey, string value) {
+
+	}
+};
